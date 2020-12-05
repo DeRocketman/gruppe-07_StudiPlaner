b6b63ca7900dd9d014996ac5a7d2b4f031a720a5
Louis Grümmer <louis.gruemmer@stud.th-luebeck.de>
2020-12-05 18:59:01 +0100

    LG erstellen eines Canvaselement für unseren Hintergrund

---
+++ public/js/canvas.js
@@ -0,0 +1,110 @@
+    const background = document.querySelector('canvas');
+
+    background.width = window.innerWidth;
+    background.height = window.innerHeight;
+
+    const godparticle = background.getContext('2d')
+
+    const mouse ={
+    x: undefined,
+    y: undefined
+}
+
+    const maxRadius = 30;
+    const minRadius = 2;
+    const circleCount = 300;
+    /*
+      Farbschema:
+      cookieCrema: #ABA27D
+      navBlau: #C3F7F4
+      sandGeld: #F8EFC8
+      zartRosé: #F5D5F0
+      dunkelRosa: #AB63A0
+     */
+    const colorArray =['#ABA27D', '#C3F7F4', '#F8EFC8', '#F5D5F0', '#AB63A0'];
+
+
+window.addEventListener('mousemove',function (event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+function Circle(x, y, velociraptorX, velociraptorY, radius){
+    this.x = x;
+    this.y = y;
+    this.velociraptorX = velociraptorX;
+    this.velociraptorY = velociraptorY;
+    this.radius = radius;
+    this.minRadius = minRadius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+    //erzeugt einen Kreis auf dem Bildschirm
+    this.generate = function () {
+        godparticle.beginPath();
+        godparticle.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        godparticle.fillStyle = this.color;
+        godparticle.fill();
+    }
+
+    this.movement = function (){
+        //*damit die Kreise auch im Bild bleiben :P
+        // *(+/-this.radius damit der Rand des Kreises vom Bildschirmrand abprallt und nicht der Mittelpunkt)
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+            this.velociraptorX = -this.velociraptorX;
+        }
+        if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+            this.velociraptorY = -this.velociraptorY;
+        }
+        //*zufällige Geschwindigkeit mit der sich die Kreise in x und y Richtung bewegen
+        this.x += this.velociraptorX;
+        this.y += this.velociraptorY;
+
+        //Interaktivität mit der Maus
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+            && mouse.y -this.y < 50 && mouse.y - this.y > -50
+            ){
+            if(this.radius < maxRadius){
+                this.radius += 1;
+            }
+        } else if (this.radius > this.minRadius) {
+            this.radius -= 1;
+        }
+        this.generate();
+    }
+}
+
+
+
+    const circleArray = [];
+
+    //erzeugt die angegebene Anzahl an Kreisen mit einer zufälligen Größe, Bewegungsgeschwindigkeit
+    //und Position und speichert sie in einem Array.
+
+    for (let i = 0; i < circleCount; i++){
+        const radius = Math.random() * 3 +1;
+        //*damit die Kreise nicht am Bildschirmrand "steckenbleiben"
+        const x = Math.random() * (innerWidth - radius * 2) + radius;
+        const y = Math.random() * (innerHeight - radius * 2) + radius;
+        const velociraptorX = (Math.random() - 0.5); //*höchster Wert Math.random=1-0.5=0.5, kleinster Wert Math.random=0-0.5=-0.5
+        const velociraptorY = (Math.random() - 0.5);
+        //*die zufälligen Werte werden übergeben um bei jedem For-Loop ein neues Circle Objekt zu erzeugen
+        circleArray.push(new Circle(x, y, velociraptorX, velociraptorY, radius));
+    }
+
+
+    function magic() {
+        requestAnimationFrame(magic);
+        //*damit die Kreise sich "nur" bewegen und keine Striche erzeugen
+        godparticle.clearRect(0, 0, innerWidth, innerHeight);
+
+        for (let i = 0; i < circleArray.length; i++){
+            circleArray[i].movement();
+        }
+
+
+    }
+
+    magic();
+
+
+
+    //Velociraptor wegen velocity^^
