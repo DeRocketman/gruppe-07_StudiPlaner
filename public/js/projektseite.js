4e08d4ff57897b7e4068dd121d845cafe0ace674
DS <Dirk.Stricker@stud.th-luebeck.de>
2020-12-08 23:22:59 +0100

    ds: object localstorage angelegt, next step: dataviewconnector basteln Zeile 560 gehts los :-)

--- public/js/projektseite.js
+++ public/js/projektseite.js
@@ -69,25 +69,25 @@ let deleter = {
         document.getElementById("aufgabe_3").className ="elementOFF";
     }
 }
-//Projektspeicher 1-5:
+//Projekte 1-5 JASON:
 let projekt1 = {
     //Grunddaten
     "projektnummer" : 1,
-    "aktiviert" : false,
-    "projektbezeichnung" : "",
+    "aktiviert" : true,
+    "projektbezeichnung" : "PP1: Webprogrammierung",
 
     //Teilnehmerdaten <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "teilnehmer1vorname" : null,
-    "teilnehmer1name"    : null,
-    "teilnehmer1email"   : null,
+    "teilnehmer1vorname" : "Benjamin",
+    "teilnehmer1name"    : "Ansohn McDoughall",
+    "teilnehmer1email"   : "benjamim.ansohn.mcdougall@stud.th-luebeck.de",
     "teilnehmer1favorit" : true,
-    "teilnehmer2vorname" : null,
-    "teilnehmer2name"    : null,
-    "teilnehmer2email"   : null,
+    "teilnehmer2vorname" : "Kim Lara",
+    "teilnehmer2name"    : "Feller",
+    "teilnehmer2email"   : "kim.lara.feller@stud.th-luebeck.de",
     "teilnehmer2favorit" : true,
-    "teilnehmer3vorname" : null,
-    "teilnehmer3name"    : null,
-    "teilnehmer3email"   : null,
+    "teilnehmer3vorname" : "Louis",
+    "teilnehmer3name"    : "Grümmer",
+    "teilnehmer3email"   : "louis.gruemmer@stud.th-luebeck.de",
     "teilnehmer3favorit" : true,
     "teilnehmer4vorname" : null,
     "teilnehmer4name"    : null,
@@ -113,57 +113,83 @@ let projekt1 = {
     "teilnehmer9name"    : null,
     "teilnehmer9email"   : null,
     "teilnehmer9favorit" : false,
-
     //PieChart
-    //  @TODO Implementierung von Louis?
-
+    "piechartdone" : 100,
+    "piechartdo"   : 100,
+    "piecharttodo" : 200,
     //Literatur <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "literatur1" : null,
-    "literatur2" : null,
-    "literatur3" : null,
-
+    "literatur1" : "HTML 5 und CSS3, Rheinwerk",
+    "literatur2" : "JavaScript for Dummies",
+    "literatur3" : "Ist kopierter Code wirklich gestohlen?!, Ratgeber",
     // Links    <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "link1href" : null,
-    "link1text" : null,
-    "link2href" : null,
-    "link2text" : null,
+    "link1href" : "http://www.gidf.de/",
+    "link1text" : "Google ist dein Freund",
+    "link2href" : "http://www.fc-hansa.de/",
+    "link2text" : "eis.de",
     "link3href" : null,
     "link3text" : null,
-
+    //Notizen
+    "notizen"   : "Tritratralala \nDer Placeholder war lustiger",
     // Aufgaben <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "aufgabe1value" : null,
-    "aufgabe1box"   : false,
-    "aufhabe1boxtext" : null,
-    "aufgabe2value" : null,
+    "aufgabe1value" : "Projektseite abschließen",
+    "aufgabe1box"   : true,
+    "aufgabe1boxtext" : "08.12.2020",
+    "aufgabe2value" : "Impressum abschließen",
     "aufgabe2box"   : false,
-    "aufhabe2boxtext" : null,
+    "aufgabe2boxtext" : "DL: 31.12.2020",
     "aufgabe3value" : null,
     "aufgabe3box"   : false,
-    "aufhabe3boxtext" : null,
-
-    //  @TODO SPEICHER/LOAD-Funktion implementieren
+    "aufgabe3boxtext" : null,
+    // Visible-Elements (Bedingt durch Klassen)
+    //Teilnehmer
+    "tn1klasse"     : "elementON",
+    "tn2klasse"     : "elementON",
+    "tn3klasse"     : "elementON",
+    "tn4klasse"     : "elementOFF",
+    "tn5klasse"     : "elementOFF",
+    "tn6klasse"     : "elementOFF",
+    "tn7klasse"     : "elementOFF",
+    "tn8klasse"     : "elementOFF",
+    "tn9klasse"     : "elementOFF",
+    //Piechart
+    "piechartklasse": "elementON",
+    //Literatur
+    "lit1klasse"    : "elementON",
+    "lit2klasse"    : "elementON",
+    "lit3klasse"    : "elementON",
+    //Links
+    "link1klasse"   : "elementON",
+    "link2klasse"   : "elementON",
+    "link3klasse"   : "elementOFF",
+    //
+    "aufgabe1klasse"      : "elementON",
+    "aufgabe1boxklasse"   : "elementON",
+    "aufgabe2klasse"      : "elementON",
+    "aufgabe2boxklasse"   : "elementON",
+    "aufgabe3klasse"      : "elementOFF",
+    "aufgabe3boxklasse"   : "elementOFF",
 }
 let projekt2 = {
+    //Grunddaten
     "projektnummer" : 2,
     "aktiviert" : false,
-    "projektbezeichnung" : "",
-
+    "projektbezeichnung" : "PP2: Aufstieg 2020",
     //Teilnehmerdaten <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "teilnehmer1vorname" : null,
-    "teilnehmer1name"    : null,
-    "teilnehmer1email"   : null,
+    "teilnehmer1vorname" : "Hans A.",
+    "teilnehmer1name"    : "Rostock",
+    "teilnehmer1email"   : "info@hansa.de",
     "teilnehmer1favorit" : true,
-    "teilnehmer2vorname" : null,
-    "teilnehmer2name"    : null,
-    "teilnehmer2email"   : null,
+    "teilnehmer2vorname" : "Mike",
+    "teilnehmer2name"    : "Werner",
+    "teilnehmer2email"   : "mike.werner@fukhuhila.de",
     "teilnehmer2favorit" : true,
-    "teilnehmer3vorname" : null,
-    "teilnehmer3name"    : null,
-    "teilnehmer3email"   : null,
+    "teilnehmer3vorname" : "Werner",
+    "teilnehmer3name"    : "Beinhart",
+    "teilnehmer3email"   : "dasmusskesseln@boelkstoff.de",
     "teilnehmer3favorit" : true,
-    "teilnehmer4vorname" : null,
-    "teilnehmer4name"    : null,
-    "teilnehmer4email"   : null,
+    "teilnehmer4vorname" : "Martin",
+    "teilnehmer4name"    : "Piekenhagen",
+    "teilnehmer4email"   : "Martin.Piekenhagen@hansa.de",
     "teilnehmer4favorit" : false,
     "teilnehmer5vorname" : null,
     "teilnehmer5name"    : null,
@@ -185,39 +211,65 @@ let projekt2 = {
     "teilnehmer9name"    : null,
     "teilnehmer9email"   : null,
     "teilnehmer9favorit" : false,
-
     //PieChart
-    //  @TODO Implementierung PIECHART von Louis?
-
+    "piechartdone" : 100,
+    "piechartdo"   : 100,
+    "piecharttodo" : 200,
     //Literatur <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "literatur1" : null,
-    "literatur2" : null,
-    "literatur3" : null,
-
+    "literatur1" : "Bundesligaaufsteiger leicht gemacht",
+    "literatur2" : "11 Freunde",
+    "literatur3" : "Erfolgreich im Sport",
     // Links    <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "link1href" : null,
-    "link1text" : null,
-    "link2href" : null,
-    "link2text" : null,
+    "link1href" : "http://www.hansa.de",
+    "link1text" : "FC HANSA",
+    "link2href" : "https://www.youtube.com/watch?v=fCZVL_8D048",
+    "link2text" : "Jerusalema",
     "link3href" : null,
     "link3text" : null,
-
     // Aufgaben <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
-    "aufgabe1value" : null,
+    "aufgabe1value" : "Tabellenerster nach Hinrunde",
     "aufgabe1box"   : false,
-    "aufhabe1boxtext" : null,
-    "aufgabe2value" : null,
+    "aufgabe1boxtext" : "DL: 23.12.2020",
+    "aufgabe2value" : "Tabellenerster nach Rückrunde",
     "aufgabe2box"   : false,
-    "aufhabe2boxtext" : null,
-    "aufgabe3value" : null,
-    "aufgabe3box"   : false,
-    "aufhabe3boxtext" : null,
+    "aufgabe2boxtext" : "DL: 30.07.2021",
+    "aufgabe3value" : "Party auf dem Rathausbalkon",
+    "aufgabe3box"   : true,
+    "aufgabe3boxtext" : "01.12.2012",
+    // Visible-Elements (Bedingt durch Klassen)
+    //Teilnehmer
+    "tn1klasse"     : "elementON",
+    "tn2klasse"     : "elementON",
+    "tn3klasse"     : "elementON",
+    "tn4klasse"     : "elementON",
+    "tn5klasse"     : "elementOFF",
+    "tn6klasse"     : "elementOFF",
+    "tn7klasse"     : "elementOFF",
+    "tn8klasse"     : "elementOFF",
+    "tn9klasse"     : "elementOFF",
+    //Piechart
+    "piechartklasse": "elementON",
+    //Literatur
+    "lit1klasse"    : "elementON",
+    "lit2klasse"    : "elementON",
+    "lit3klasse"    : "elementON",
+    //Links
+    "link1klasse"   : "elementON",
+    "link2klasse"   : "elementON",
+    "link3klasse"   : "elementOFF",
+    //
+    "aufgabe1klasse"      : "elementON",
+    "aufgabe1boxklasse"   : "elementON",
+    "aufgabe2klasse"      : "elementON",
+    "aufgabe2boxklasse"   : "elementON",
+    "aufgabe3klasse"      : "elementON",
+    "aufgabe3boxklasse"   : "elementON",
 }
 let projekt3 = {
+    //Grunddaten
     "projektnummer" : 3,
     "aktiviert" : false,
     "projektbezeichnung" : "",
-
     //Teilnehmerdaten <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "teilnehmer1vorname" : null,
     "teilnehmer1name"    : null,
@@ -255,15 +307,14 @@ let projekt3 = {
     "teilnehmer9name"    : null,
     "teilnehmer9email"   : null,
     "teilnehmer9favorit" : false,
-
     //PieChart
-    //  @TODO Implementierung PIECHART von Louis?
-
+    "piechartdone" : 0,
+    "piechartdo"   : 0,
+    "piecharttodo" : 0,
     //Literatur <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "literatur1" : null,
     "literatur2" : null,
     "literatur3" : null,
-
     // Links    <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "link1href" : null,
     "link1text" : null,
@@ -271,23 +322,49 @@ let projekt3 = {
     "link2text" : null,
     "link3href" : null,
     "link3text" : null,
-
     // Aufgaben <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "aufgabe1value" : null,
     "aufgabe1box"   : false,
-    "aufhabe1boxtext" : null,
+    "aufgabe1boxtext" : null,
     "aufgabe2value" : null,
     "aufgabe2box"   : false,
-    "aufhabe2boxtext" : null,
+    "aufgabe2boxtext" : null,
     "aufgabe3value" : null,
     "aufgabe3box"   : false,
-    "aufhabe3boxtext" : null,
+    "aufgabe3boxtext" : null,
+    // Visible-Elements (Bedingt durch Klassen)
+    //Teilnehmer
+    "tn1klasse"     : "elementOFF",
+    "tn2klasse"     : "elementOFF",
+    "tn3klasse"     : "elementOFF",
+    "tn4klasse"     : "elementOFF",
+    "tn5klasse"     : "elementOFF",
+    "tn6klasse"     : "elementOFF",
+    "tn7klasse"     : "elementOFF",
+    "tn8klasse"     : "elementOFF",
+    "tn9klasse"     : "elementOFF",
+    //Piechart
+    "piechartklasse": "elementOFF",
+    //Literatur
+    "lit1klasse"    : "elementOFF",
+    "lit2klasse"    : "elementOFF",
+    "lit3klasse"    : "elementOFF",
+    //Links
+    "link1klasse"   : "elementOFF",
+    "link2klasse"   : "elementOFF",
+    "link3klasse"   : "elementOFF",
+    //
+    "aufgabe1klasse"      : "elementOFF",
+    "aufgabe1boxklasse"   : "elementOFF",
+    "aufgabe2klasse"      : "elementOFF",
+    "aufgabe2boxklasse"   : "elementOFF",
+    "aufgabe3klasse"      : "elementOFF",
+    "aufgabe3boxklasse"   : "elementOFF",
 }
 let projekt4 = {
     "projektnummer" :4,
     "aktiviert" : false,
     "projektbezeichnung" : "",
-
     //Teilnehmerdaten <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "teilnehmer1vorname" : null,
     "teilnehmer1name"    : null,
@@ -325,15 +402,14 @@ let projekt4 = {
     "teilnehmer9name"    : null,
     "teilnehmer9email"   : null,
     "teilnehmer9favorit" : false,
-
     //PieChart
-    //  @TODO Implementierung PIECHART von Louis?
-
+    "piechartdone" : 0,
+    "piechartdo"   : 0,
+    "piecharttodo" : 0,
     //Literatur <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "literatur1" : null,
     "literatur2" : null,
     "literatur3" : null,
-
     // Links    <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "link1href" : null,
     "link1text" : null,
@@ -341,7 +417,6 @@ let projekt4 = {
     "link2text" : null,
     "link3href" : null,
     "link3text" : null,
-
     // Aufgaben <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "aufgabe1value" : null,
     "aufgabe1box"   : false,
@@ -351,13 +426,39 @@ let projekt4 = {
     "aufhabe2boxtext" : null,
     "aufgabe3value" : null,
     "aufgabe3box"   : false,
-    "aufhabe3boxtext" : null,
+    "aufgabe3boxtext" : null,
+    "tn1klasse"     : "elementOFF",
+    "tn2klasse"     : "elementOFF",
+    "tn3klasse"     : "elementOFF",
+    "tn4klasse"     : "elementOFF",
+    "tn5klasse"     : "elementOFF",
+    "tn6klasse"     : "elementOFF",
+    "tn7klasse"     : "elementOFF",
+    "tn8klasse"     : "elementOFF",
+    "tn9klasse"     : "elementOFF",
+    //Piechart
+    "piechartklasse": "elementOFF",
+    //Literatur
+    "lit1klasse"    : "elementOFF",
+    "lit2klasse"    : "elementOFF",
+    "lit3klasse"    : "elementOFF",
+    //Links
+    "link1klasse"   : "elementOFF",
+    "link2klasse"   : "elementOFF",
+    "link3klasse"   : "elementOFF",
+    //
+    "aufgabe1klasse"      : "elementOFF",
+    "aufgabe1boxklasse"   : "elementOFF",
+    "aufgabe2klasse"      : "elementOFF",
+    "aufgabe2boxklasse"   : "elementOFF",
+    "aufgabe3klasse"      : "elementOFF",
+    "aufgabe3boxklasse"   : "elementOFF",
 }
 let projekt5 = {
+    //Grunddaten
     "projektnummer" : 5,
     "aktiviert" : false,
     "projektbezeichnung" : "",
-
     //Teilnehmerdaten <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "teilnehmer1vorname" : null,
     "teilnehmer1name"    : null,
@@ -395,15 +496,14 @@ let projekt5 = {
     "teilnehmer9name"    : null,
     "teilnehmer9email"   : null,
     "teilnehmer9favorit" : false,
-
     //PieChart
-    //  @TODO Implementierung PIECHART von Louis?
-
+    "piechartdone" : 0,
+    "piechartdo"   : 0,
+    "piecharttodo" : 0,
     //Literatur <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "literatur1" : null,
     "literatur2" : null,
     "literatur3" : null,
-
     // Links    <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "link1href" : null,
     "link1text" : null,
@@ -411,19 +511,54 @@ let projekt5 = {
     "link2text" : null,
     "link3href" : null,
     "link3text" : null,
-
     // Aufgaben <-Sammlung, eventuell in einem bzw. mehreren Arrays zusammenfassen
     "aufgabe1value" : null,
     "aufgabe1box"   : false,
-    "aufhabe1boxtext" : null,
+    "aufgabe1boxtext" : null,
     "aufgabe2value" : null,
     "aufgabe2box"   : false,
-    "aufhabe2boxtext" : null,
+    "aufgabe2boxtext" : null,
     "aufgabe3value" : null,
     "aufgabe3box"   : false,
-    "aufhabe3boxtext" : null,
+    "aufgabe3boxtext" : null,
+    "tn1klasse"     : "elementOFF",
+    "tn2klasse"     : "elementOFF",
+    "tn3klasse"     : "elementOFF",
+    "tn4klasse"     : "elementOFF",
+    "tn5klasse"     : "elementOFF",
+    "tn6klasse"     : "elementOFF",
+    "tn7klasse"     : "elementOFF",
+    "tn8klasse"     : "elementOFF",
+    "tn9klasse"     : "elementOFF",
+    //Piechart
+    "piechartklasse": "elementOFF",
+    //Literatur
+    "lit1klasse"    : "elementOFF",
+    "lit2klasse"    : "elementOFF",
+    "lit3klasse"    : "elementOFF",
+    //Links
+    "link1klasse"   : "elementOFF",
+    "link2klasse"   : "elementOFF",
+    "link3klasse"   : "elementOFF",
+    //
+    "aufgabe1klasse"      : "elementOFF",
+    "aufgabe1boxklasse"   : "elementOFF",
+    "aufgabe2klasse"      : "elementOFF",
+    "aufgabe2boxklasse"   : "elementOFF",
+    "aufgabe3klasse"      : "elementOFF",
+    "aufgabe3boxklasse"   : "elementOFF",
 }
-
+//Speicherfunktionen der Projekte in localStorage
+window.localStorage.setItem('projekt1', JSON.stringify(projekt1));
+window.localStorage.setItem('projekt2', JSON.stringify(projekt2));
+window.localStorage.setItem('projekt3', JSON.stringify(projekt3));
+window.localStorage.setItem('projekt4', JSON.stringify(projekt4));
+window.localStorage.setItem('projekt5', JSON.stringify(projekt5));
+//Diese Funktion verbindet Daten der Projekte mit der Ansicht
+//TODO code den/die Connector
+function dataViewConnector1(){
+    //MEGA
+}
 //Mailfunktionenen
 let personAmail;
 let personBmail;
@@ -505,7 +640,7 @@ function SchalteFreiFunktion() {
     document.getElementById("projektabrruch").className = "settingON";
 }
 //3. Diese Funktion erstellt ein neues Projekt (Usersicht)\intern werden Projekte vorerst lediglich visible
-var projektAnzahl = 0;
+var projektAnzahl = 2;
 document.getElementById("projektuebernahme").addEventListener("click", projektAnlegeFunktion);
 function projektAnlegeFunktion(){
     projektAnzahl++;
@@ -514,24 +649,28 @@ function projektAnlegeFunktion(){
             case 1:
                 projekt1.aktiviert = true;
                 projekt1.projektbezeichnung = document.getElementById("inputFeld").value;
-                deleter.anleger();
+                //deleter.anleger(); Pseudoprojekt 1
                 break;
             case 2:
                 projekt2.aktiviert = true;
                 projekt2.projektbezeichnung = document.getElementById("inputFeld").value;
-                deleter.anleger();
+                //deleter.anleger(); Pseudoprojekt 2
+                break;
             case 3:
                 projekt3.aktiviert = true;
                 projekt3.projektbezeichnung = document.getElementById("inputFeld").value;
                 deleter.anleger();
+                break;
             case 4:
                 projekt4.aktiviert = true;
                 projekt4.projektbezeichnung = document.getElementById("inputFeld").value;
                 deleter.anleger();
+                break;
             case 5:
                 projekt5.aktiviert = true;
                 projekt5.projektbezeichnung = document.getElementById("inputFeld").value;
                 deleter.anleger();
+                break;
         }
     }
     //nach Anlegen des Projekts bzw. Abbruch wird alles wieder invisible -> Rückkehr zur normalen Ansicht
@@ -604,4 +743,9 @@ function mailsetokifunction() {
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
-
+//Loader für Später das wird Mega!
+$(document).ready(JSON.parse(window.localStorage.getItem('projekt1')));
+$(document).ready(JSON.parse(window.localStorage.getItem('projekt2')));
+$(document).ready(JSON.parse(window.localStorage.getItem('projekt3')));
+$(document).ready(JSON.parse(window.localStorage.getItem('projekt4')));
+$(document).ready(JSON.parse(window.localStorage.getItem('projekt5')));
