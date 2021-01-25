97b4feaa446fe056a29793a0a6555d2dd57e49a3
bam <bam@wps.de>
2021-01-25 15:21:37 +0100

    Kommentare für die Klassen in der Domäne des Kalenders

--- public/js/Kalender/domain/Termin.js
+++ public/js/Kalender/domain/Termin.js
@@ -1,7 +1,12 @@
 /*
-    Stellt einen Termin - Tag im Kalender dar
+    Stellt einen Termin - Tag im Kalender - dar
+    Ein Termin hat ein Datum, eine Art der Veranstaltung, eine Menge von Teilnehmerinnen, es kann ein
+    Einzeltermin oder eine Terminserie sein und es können weiter Informationen in Form eines Freitextes angeheftet
+    werden.
 
-    Author: Ben Ansohn McDougall
+    Der Termin bietet eine toString Methode um sich die Informationen zum Termin anzeigen lassen zu können.
+
+    Autor: Ben Ansohn McDougall
  */
 export class Termin {
     constructor(datum = '2021-12-04', veranstaltungsArt = 'Vorlesung', teilnehmer = [],
--- public/js/Kalender/domain/jahr.js
+++ public/js/Kalender/domain/jahr.js
@@ -1,5 +1,10 @@
 /*
     Ein Kalender Jahr, aggregat aller Monate und entsprechender Termine
+
+    Man kann sich die einzelnen Monatsobjekte und die Jahreszahl anzeigen lassen.
+    Ursprünglich gedacht um größere Terminübersichten einfach darstellen zu können.
+    Aus Zeitgründen nicht mehr zum Einsatz gekommen.
+
     Author: Benjamin Ansohn McDougall
  */
 import {Monat} from "./monat.js";
